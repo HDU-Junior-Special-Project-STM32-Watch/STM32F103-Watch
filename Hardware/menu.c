8b73c69b3acd5a7417473fb4fe70a0e0e23ee6c8
YINGYYUN <756209834@qq.com>
2026-01-21 23:42:02 +0800

    2026.1.21.23:41(YXZ):完成[1-4]

--- Hardware/menu.c
+++ Hardware/menu.c
@@ -7,6 +7,7 @@
 #include "Key.h"
 #include "SetTime.h"					//日期时间设置模块
 #include "SetBrightness.h"				//亮度设置模块
+#include "Menu.h"
 
 /*--------------------[S] 外设初始化 [S]--------------------*/
 
@@ -242,8 +243,26 @@ void Set_Selection(uint8_t move_flag, uint8_t Pre_Selection, uint8_t Targrt_Sele
 	if (move_flag == 1)
 	{
 		pre_selection = Pre_Selection;
-		targrt_selection = Targrt_Selection;
-		Menu_Animation();
+		targrt_selection = Targrt_Selection;	
+	}
+	//此函数外置，保证从功能中退出后能重新显示菜单界面
+	Menu_Animation();
+}
+
+//转场函数（图标向下移动）
+void MenuToFunction(void)
+{
+	for (uint8_t i = 0;i <= 6;i ++)
+	{
+		OLED_Clear();
+		if (pre_selection >= 1)
+		{
+			OLED_ShowImage(x_pre - 48, 16 + 8*i, 32, 32, Menu_Graph[pre_selection - 1]);
+		}
+		OLED_ShowImage(x_pre, 16 + 8*i, 32, 32, Menu_Graph[pre_selection]);
+		OLED_ShowImage(x_pre + 48, 16 + 8*i, 32, 32, Menu_Graph[pre_selection + 1]);
+		
+		OLED_Update();
 	}
 }
 
@@ -292,7 +311,7 @@ int Menu(void)
 		//返回上一级菜单
 		if (menu_falg_temp == 1){return 0;}
 		//
-		else if (menu_falg_temp == 2){}
+		else if (menu_falg_temp == 2){MenuToFunction();StopWatch();}
 		else if (menu_falg_temp == 3){}
 		else if (menu_falg_temp == 4){}
 		else if (menu_falg_temp == 5){}
@@ -313,3 +332,135 @@ int Menu(void)
 	}
 }
 /*--------------------[E] 滑动菜单界面 [E]--------------------*/
+
+
+/*--------------------[S] 秒表界面 [S]--------------------*/
+
+uint8_t hour, min, sec;
+
+void Show_StopWatch_UI(void)
+{
+	OLED_ShowImage(0, 0, 16, 16, Return);
+	OLED_Printf(32, 20, OLED_8X16, "%02d:%02d:%02d", hour, min, sec);
+	OLED_ShowString(8, 44, "开始", OLED_8X16);
+	OLED_ShowString(48, 44, "停止", OLED_8X16);
+	OLED_ShowString(88, 44, "清除", OLED_8X16);
+}
+
+//开始计时标志位；1开始，0停止
+uint8_t start_timing_flag = 0;
+
+void StopWatch_Tick(void)
+{
+	//分频用计数
+	static uint16_t Count;
+	Count ++;
+	
+	if (Count >= 1000)
+	{
+		Count = 0;
+		if (start_timing_flag == 1)
+		{
+			sec ++;
+			if (sec >= 60)
+			{
+				sec = 0;
+				min ++;
+				if (min >= 60)
+				{
+					min = 0;
+					hour ++;
+					if (hour > 99)
+					{
+						hour = 0;
+					}
+				}
+			}
+		}
+	}
+	
+	
+}
+
+//秒表界面选项标志位
+uint8_t stopwatch_flag = 1;
+
+int StopWatch(void)
+{
+	while(1)
+	{
+		//存储确认键被按下时stopwatch_flag的值的临时变量，默认为无效值0
+		uint8_t stopwatch_flag_temp = 0;
+		
+		//上键
+		if (Key_Check(KEY_NAME_UP,KEY_SINGLE))
+		{
+			stopwatch_flag --;
+			if (stopwatch_flag <=0) stopwatch_flag = 4;
+		}
+		//下键
+		else if (Key_Check(KEY_NAME_DOWN,KEY_SINGLE))
+		{
+			stopwatch_flag ++;
+			if (stopwatch_flag >= 5) stopwatch_flag = 1;
+		}
+		//确认键
+		else if (Key_Check(KEY_NAME_COMFIRM,KEY_SINGLE))
+		{
+			//清屏OLED，准备跳转
+			OLED_Clear();
+			OLED_Update();
+			
+			stopwatch_flag_temp = setflag;
+		}
+		
+		//返回上一级菜单
+		if (stopwatch_flag_temp == 1){return 0;}
+		
+		switch(stopwatch_flag)
+		{
+			//光标在"返回"键处
+			case 1:
+			{
+				Show_StopWatch_UI();
+				OLED_ReverseArea(0, 0, 16, 16);
+				OLED_Update();
+				
+				break;
+			}
+			//光标在"开始"键处
+			case 2:
+			{
+				Show_StopWatch_UI();
+				start_timing_flag = 1;
+				OLED_ReverseArea(8, 44, 32, 16);
+				OLED_Update();
+				
+				break;
+			}
+			//光标在"停止"键处
+			case 3:
+			{
+				Show_StopWatch_UI();
+				start_timing_flag = 0;
+				OLED_ReverseArea(48, 44, 32, 16);
+				OLED_Update();
+				
+				break;
+			}	
+			//光标在"清除"键处
+			case 4:
+			{
+				Show_StopWatch_UI();
+				start_timing_flag = 0;
+				hour = min =sec = 0;
+				OLED_ReverseArea(88, 44, 32, 16);
+				OLED_Update();
+				
+				break;
+			}	
+		}
+	}
+}
+
+/*--------------------[E] 秒表界面 [E]--------------------*/
--- Hardware/menu.h
+++ Hardware/menu.h
@@ -8,5 +8,7 @@ void Show_Clock_UI(void);
 int First_Page_Clock(void);
 int SettingPage(void);
 int Menu(void);
+void StopWatch_Tick(void);
+int StopWatch(void);
 
 #endif
